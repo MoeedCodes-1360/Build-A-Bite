1bdbca2be3c3f0d49446bcb5d6b86a8b392a5717
nn <mmoon8941@gmail.com>
2026-09-01 10:36:09 +0500

    debugging

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,39 +1,39 @@
-import {  useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import Input from "./Input"
-import Rating from "./Rating"
+import Input from "./Input";
+import Rating from "./Rating";
 
 function Review() {
-  function Reviews() {
-    return (
   const [reviews, setReviews] = useState(() => {
     const savedReviews = localStorage.getItem("reviews");
 
-    return savedReviews ? JSON.parse(savedReviews) : [];)
+    return savedReviews ? JSON.parse(savedReviews) : [];
   });
 
+  let [rating, setRating] = useState(0);
+  let [review, setReview] = useState("");
+  let [name, setName] = useState("");
 
-  let [rating, setRating] = useState(0)
-  // let formRef=useRef(null)
-  let  [review, setReview] = useState("")
-  let [name, setName] = useState("")
-    useEffect(() => {
+  useEffect(() => {
     localStorage.setItem("reviews", JSON.stringify(reviews));
   }, [reviews]);
+
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
     const newReview = {
       id: Date.now(),
       name: name,
       review: review,
-      rating: Number(rating)
+      rating: Number(rating),
     };
-    setReviews([...reviews, newReview]);
-    setRating(0)
-    setReview ("")
-    setName("")
 
-  } 
+    setReviews([...reviews, newReview]);
+
+    setRating(0);
+    setReview("");
+    setName("");
+  };
 
   return (
     <div className="min-h-screen w-full flex justify-center items-center px-6">
@@ -50,7 +50,6 @@ function Review() {
           name="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          // ref={formRef}
           placeholder="Enter your name (optional)"
           className="w-full border border-white/30 text-white rounded-md p-3"
         />
@@ -65,6 +64,7 @@ function Review() {
 
         <div>
           <p className="text-white mb-2">Your rating</p>
+
           <Rating
             value={rating}
             onChange={setRating}
@@ -78,7 +78,7 @@ function Review() {
         />
       </form>
     </div>
-  )
+  );
 }
-}
- export default Review
+
+export default Review;
